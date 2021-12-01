1f3010d8502bbbdf124183dafdda1c0c5e6f3562
Glen Nelson <glen.nelson.1@gmail.com>
2021-11-30 23:31:12 -0700

    Day01: Correct answer to part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,15 @@
 use day01::load;
 use day01::parse_string;
 use day01::puzzle_a;
+use day01::puzzle_b;
 
 fn main() {
     let contents = load();
     let depths = parse_string(&contents);
 
-    let value = puzzle_a(depths);
-    println!("Answer to 1st question: {}", value)
+    let value = puzzle_a(&depths);
+    println!("Answer to 1st question: {}", value);
+
+    let value_b = puzzle_b(&depths);
+    println!("Answer to 2nd question: {}", value_b);
 }
