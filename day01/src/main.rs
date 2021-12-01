9b084a46a74cc152ae3e380d470a99567119132c
Glen Nelson <glen.nelson.1@gmail.com>
2021-11-30 23:07:31 -0700

    Day01: solution to A
    The solution here is a simple loop. Functional programming is used
    for most of the puzzle input steps, but the puzzle logic is left to
    be clear.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,11 @@
+use day01::load;
+use day01::parse_string;
+use day01::puzzle_a;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = load();
+    let depths = parse_string(&contents);
+
+    let value = puzzle_a(depths);
+    println!("Answer to 1st question: {}", value)
 }
