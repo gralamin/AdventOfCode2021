4a0af9144f7bbb43925d10a24df24d012f3ee63e
Glen Nelson <glen.nelson.1@gmail.com>
2021-11-30 18:46:53 -0700

    Add Hello world in rust

---
+++ helloworld/src/lib.rs
@@ -0,0 +1,26 @@
+/// First line is a short summary describing function.
+///
+/// The next lines present detailed documentation. Code blocks start with
+/// triple backquotes and have implicit `fn main()` inside
+/// and `extern crate <cratename>`. Assume we're testing `doccomments` crate:
+///
+/// ```
+/// let result = helloworld::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+}
