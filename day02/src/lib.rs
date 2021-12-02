9cbb890aa5295a4eda7a82f4a246e3849dd2df51
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-01 23:43:55 -0700

    Convert day02 to use shared code

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,25 +1,25 @@
-mod filelib;
-mod sub;
+extern crate filelib;
+extern crate submarinelib;
 
-use crate::sub::AimingSub;
-use crate::sub::AimlessSub;
-use crate::sub::SubDirection;
-use crate::sub::SubPosition;
+use submarinelib::AimingSub;
+use submarinelib::AimlessSub;
+use submarinelib::SubDirection;
+use submarinelib::SubPosition;
 
-pub use crate::filelib::load_no_blanks;
-pub use crate::sub::directional_commands_from_strs;
+pub use filelib::load_no_blanks;
+pub use submarinelib::directional_commands_from_strs;
 
 /// Move the sub according to the directions.
 ///
 /// Assumes the directions have already been parsed.
 ///
 /// ```
-/// let steps = vec![(day02::SubDirection::Forward, 5),
-///                  (day02::SubDirection::Down, 5),
-///                  (day02::SubDirection::Forward, 8),
-///                  (day02::SubDirection::Up, 3),
-///                  (day02::SubDirection::Down, 8),
-///                  (day02::SubDirection::Forward, 2)];
+/// let steps = vec![(submarinelib::SubDirection::Forward, 5),
+///                  (submarinelib::SubDirection::Down, 5),
+///                  (submarinelib::SubDirection::Forward, 8),
+///                  (submarinelib::SubDirection::Up, 3),
+///                  (submarinelib::SubDirection::Down, 8),
+///                  (submarinelib::SubDirection::Forward, 2)];
 /// assert_eq!(day02::puzzle_a(steps), 150);
 /// ```
 pub fn puzzle_a(steps: Vec<(SubDirection, i32)>) -> i32 {
@@ -44,12 +44,12 @@ pub fn puzzle_a(steps: Vec<(SubDirection, i32)>) -> i32 {
 /// Assumes the directions have already been parsed.
 ///
 /// ```
-/// let steps = vec![(day02::SubDirection::Forward, 5),
-///                  (day02::SubDirection::Down, 5),
-///                  (day02::SubDirection::Forward, 8),
-///                  (day02::SubDirection::Up, 3),
-///                  (day02::SubDirection::Down, 8),
-///                  (day02::SubDirection::Forward, 2)];
+/// let steps = vec![(submarinelib::SubDirection::Forward, 5),
+///                  (submarinelib::SubDirection::Down, 5),
+///                  (submarinelib::SubDirection::Forward, 8),
+///                  (submarinelib::SubDirection::Up, 3),
+///                  (submarinelib::SubDirection::Down, 8),
+///                  (submarinelib::SubDirection::Forward, 2)];
 /// assert_eq!(day02::puzzle_b(steps), 900);
 /// ```
 pub fn puzzle_b(steps: Vec<(SubDirection, i32)>) -> i32 {
--- day02/src/sub/movement.rs
+++
@@ -1,5 +0,0 @@
-pub trait SubPosition {
-    fn forward(&mut self, by: i32);
-    fn down(&mut self, by: i32);
-    fn up(&mut self, by: i32);
-}
