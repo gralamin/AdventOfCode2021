7f357c77c271d8d507a82fe48217f0190fef69de
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-02 20:54:28 -0700

    day03: template

---
+++ day03/src/lib.rs
@@ -0,0 +1,17 @@
+
+
+/// puzzle_a
+///
+/// ```
+/// let sample_input = []
+/// assert_eq!(day03::puzzle_a(sample_input), 900);
+/// ```
+pub fn puzzle_a() {};
+
+/// puzzle_b
+///
+/// ```
+/// let sample_input = []
+/// assert_eq!(day03::puzzle_b(sample_input), 900);
+/// ```
+pub fn puzzle_b() {};
