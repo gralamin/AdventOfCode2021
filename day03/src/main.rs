4b393683950097de78a60e6cf76d07586f6b5535
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-02 20:54:28 -0700

    day03: template

---
+++ day03/src/main.rs
@@ -0,0 +1,22 @@
+//use day03::directional_commands_from_strs;
+//use day03::load_no_blanks;
+use day03::puzzle_a;
+use day03::puzzle_b;
+
+fn main() {
+    /*
+    let filename = "input";
+    let command_strs = load_no_blanks(filename);
+    let command_refs: Vec<&str> = command_strs.iter().map(AsRef::as_ref).collect();
+    let commands = directional_commands_from_strs(command_refs);
+
+    let value_a = puzzle_a(commands);
+    println!("Answer to 1st question: {}", value_a);
+
+    let command_refs_b: Vec<&str> = command_strs.iter().map(AsRef::as_ref).collect();
+    let commands_b = directional_commands_from_strs(command_refs_b);
+
+    let value_b = puzzle_b(commands_b);
+    println!("Answer to 2nd question: {}", value_b);
+    */
+}
