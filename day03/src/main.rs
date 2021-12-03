7d05c66b5734faf38fe4aabaa2e7765e874da11a
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-02 23:19:32 -0700

    day03: Solution to part b

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -11,11 +11,7 @@ fn main() {
     let value_a = puzzle_a(diagnostic_refs);
     println!("Answer to 1st question: {}", value_a);
 
-    /*
-    let command_refs_b: Vec<&str> = command_strs.iter().map(AsRef::as_ref).collect();
-    let commands_b = directional_commands_from_strs(command_refs_b);
-
-    let value_b = puzzle_b(commands_b);
+    let diagnostic_refs_b: Vec<&str> = diagnostic_strs.iter().map(AsRef::as_ref).collect();
+    let value_b = puzzle_b(diagnostic_refs_b);
     println!("Answer to 2nd question: {}", value_b);
-    */
 }
