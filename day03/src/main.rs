6d7cbe84c84d75842b027f8163c0707a78debbba
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-02 22:33:22 -0700

    day03: Add solution to part one

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,18 +1,17 @@
 //use day03::directional_commands_from_strs;
-//use day03::load_no_blanks;
+use day03::load_no_blanks;
 use day03::puzzle_a;
 use day03::puzzle_b;
 
 fn main() {
-    /*
     let filename = "input";
-    let command_strs = load_no_blanks(filename);
-    let command_refs: Vec<&str> = command_strs.iter().map(AsRef::as_ref).collect();
-    let commands = directional_commands_from_strs(command_refs);
+    let diagnostic_strs = load_no_blanks(filename);
+    let diagnostic_refs: Vec<&str> = diagnostic_strs.iter().map(AsRef::as_ref).collect();
 
-    let value_a = puzzle_a(commands);
+    let value_a = puzzle_a(diagnostic_refs);
     println!("Answer to 1st question: {}", value_a);
 
+    /*
     let command_refs_b: Vec<&str> = command_strs.iter().map(AsRef::as_ref).collect();
     let commands_b = directional_commands_from_strs(command_refs_b);
 
