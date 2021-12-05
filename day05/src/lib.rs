d43aad4cdcf884f57eb2cff0143606e2e1770e49
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-04 23:19:36 -0700

    day05: part b solution

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -16,8 +16,18 @@ pub fn parse_line_to_coords(line: &str) -> (i32, i32, i32, i32) {
     return (vec_flat[0], vec_flat[1], vec_flat[2], vec_flat[3]);
 }
 
-/// Function to get all points a line touches
 fn get_points_on_line(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<(i32, i32)> {
+    return get_points_on_line_full(x1, y1, x2, y2, false);
+}
+
+/// Function to get all points a line touches
+fn get_points_on_line_full(
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+    include_diags: bool,
+) -> Vec<(i32, i32)> {
     let mut points: Vec<(i32, i32)> = Vec::new();
     let lower_bound: i32;
     let higher_bound: i32;
@@ -45,6 +55,37 @@ fn get_points_on_line(x1: i32, y1: i32, x2: i32, y2: i32) -> Vec<(i32, i32)> {
 
         for v in lower_bound..=higher_bound {
             points.push((x1, v));
+        }
+    } else if include_diags {
+        // 45 degree diagonal case
+        // 4 possible directions: Bottom left to top right
+        if x1 < x2 && y1 > y2 {
+            // As x1 increases by one, y1 decrases by one
+            for v in x1..=x2 {
+                let y = y1 - (v - x1);
+                points.push((v, y));
+            }
+            // direction 2: top left, to bottom right
+        } else if x1 < x2 && y1 < y2 {
+            // As x1 increases by one, y1 increase by one
+            for v in x1..=x2 {
+                let y = y1 + (v - x1);
+                points.push((v, y));
+            }
+            // direction 3: Top right to bottom left
+        } else if x1 > x2 && y1 < y2 {
+            // as x2 increases by one, y2 decreases by one
+            for v in x2..=x1 {
+                let y = y2 - (v - x2);
+                points.push((v, y));
+            }
+            // Final direction, bottom right to top left
+        } else {
+            // as x2 increases by one, y2 increases by one
+            for v in x2..=x1 {
+                let y = y2 + (v - x2);
+                points.push((v, y));
+            }
         }
     }
     return points;
@@ -115,6 +156,39 @@ pub fn puzzle_a(line_pairs: &Vec<(i32, i32, i32, i32)>) -> i32 {
     return map.get_points_with_gte(2).len().try_into().unwrap();
 }
 
+/// Solution to the second puzzle.
+///
+/// Same as first, but takes into account 45 degree diagonals
+/// ```
+/// let inputs = vec![
+///                   (0, 9, 5, 9),
+///                   (8, 0, 0, 8),
+///                   (9, 4, 3, 4),
+///                   (2, 2, 2, 1),
+///                   (7, 0, 7, 4),
+///                   (6, 4, 2, 0),
+///                   (0, 9, 2, 9),
+///                   (3, 4, 1, 4),
+///                   (0, 0, 8, 8),
+///                   (5, 5, 8, 2),
+/// ];
+/// assert_eq!(day05::puzzle_b(&inputs), 12);
+/// ```
+pub fn puzzle_b(line_pairs: &Vec<(i32, i32, i32, i32)>) -> i32 {
+    let mut map = SparseHydroMap {
+        data: HashMap::new(),
+    };
+    for pos_pair in line_pairs {
+        let x1 = pos_pair.0;
+        let y1 = pos_pair.1;
+        let x2 = pos_pair.2;
+        let y2 = pos_pair.3;
+        let all_points = get_points_on_line_full(x1, y1, x2, y2, true);
+        map.mark_line(all_points);
+    }
+    return map.get_points_with_gte(2).len().try_into().unwrap();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -125,6 +199,20 @@ mod tests {
         let expected_vertical = vec![(1, 1), (1, 2), (1, 3)];
         assert_eq!(get_points_on_line(1, 1, 1, 3), expected_vertical);
         assert_eq!(get_points_on_line(9, 7, 7, 7), expected_horizontal);
+    }
+
+    #[test]
+    fn test_get_points_on_line_diagnonal() {
+        let expected_one = vec![(1, 1), (2, 2), (3, 3)];
+        let expected_two = vec![(7, 9), (8, 8), (9, 7)];
+        // top left to bottom right
+        assert_eq!(get_points_on_line_full(1, 1, 3, 3, true), expected_one);
+        // bottom left to top right
+        assert_eq!(get_points_on_line_full(9, 7, 7, 9, true), expected_two);
+        // bottom right to top left
+        assert_eq!(get_points_on_line_full(3, 3, 1, 1, true), expected_one);
+        // top right to bottom left
+        assert_eq!(get_points_on_line_full(7, 9, 9, 7, true), expected_two);
     }
 
     #[test]
@@ -150,6 +238,29 @@ mod tests {
         // Result should be:
         // 0,9   1,9,   2,9  3,4  8,4
         assert_eq!(map.get_points_with_gte(2).len(), 5);
+    }
+
+    #[test]
+    fn test_sparse_hydro_map_with_diags() {
+        let mut map = SparseHydroMap {
+            data: HashMap::new(),
+        };
+        let lines = vec![
+            get_points_on_line_full(0, 9, 5, 9, true),
+            get_points_on_line_full(8, 0, 0, 8, true),
+            get_points_on_line_full(9, 4, 3, 4, true),
+            get_points_on_line_full(2, 2, 2, 1, true),
+            get_points_on_line_full(7, 0, 7, 4, true),
+            get_points_on_line_full(6, 4, 2, 0, true),
+            get_points_on_line_full(0, 9, 2, 9, true),
+            get_points_on_line_full(3, 4, 1, 4, true),
+            get_points_on_line_full(0, 0, 8, 8, true),
+            get_points_on_line_full(5, 5, 8, 2, true),
+        ];
+        for line in lines {
+            map.mark_line(line);
+        }
+        assert_eq!(map.get_points_with_gte(2).len(), 12);
     }
 
     #[test]
--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,12 +1,17 @@
 use day05::load_no_blanks;
 use day05::parse_line_to_coords;
 use day05::puzzle_a;
+use day05::puzzle_b;
 
 fn main() {
     let filename = "input";
     let all_lines = load_no_blanks(filename);
-    let pos_pairs: Vec<(i32, i32, i32, i32)> = all_lines.iter().map(|s| parse_line_to_coords(s)).collect();
+    let pos_pairs: Vec<(i32, i32, i32, i32)> =
+        all_lines.iter().map(|s| parse_line_to_coords(s)).collect();
 
     let value_a = puzzle_a(&pos_pairs);
     println!("Solution to 1: {}", value_a);
+
+    let value_b = puzzle_b(&pos_pairs);
+    println!("Solution to 1: {}", value_b);
 }
