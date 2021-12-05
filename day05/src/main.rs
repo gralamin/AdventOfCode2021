3f6b38c31f3f67e91e497ee26fdfbf1abb5291e1
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-04 22:58:17 -0700

    day05: Part 1 fully solved

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,3 +1,12 @@
+use day05::load_no_blanks;
+use day05::parse_line_to_coords;
+use day05::puzzle_a;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load_no_blanks(filename);
+    let pos_pairs: Vec<(i32, i32, i32, i32)> = all_lines.iter().map(|s| parse_line_to_coords(s)).collect();
+
+    let value_a = puzzle_a(&pos_pairs);
+    println!("Solution to 1: {}", value_a);
 }
