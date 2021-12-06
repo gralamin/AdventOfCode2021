cc3eed28e12dde23cbbbf57ccc1764296c6a034a
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-05 22:05:14 -0700

    day06: Load input

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,3 +1,9 @@
+use day06::load;
+use day06::parse_csv_i32_lines;
+
+
 fn main() {
-    println!("Hello, world!");
+    let data = load("input");
+    let input_strings: Vec<Vec<String>> = vec![data.lines().map(|s| s.to_string()).collect()];
+    let input_ints: Vec<i32> = parse_csv_i32_lines(input_strings);
 }
