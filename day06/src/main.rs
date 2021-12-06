58c9ecc0b28456accd9f9efcfde253bc5ba499c2
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-05 22:34:43 -0700

    day06: simple simulation solution

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,9 +1,12 @@
 use day06::load;
 use day06::parse_csv_i32_lines;
-
+use day06::puzzle_a;
 
 fn main() {
     let data = load("input");
     let input_strings: Vec<Vec<String>> = vec![data.lines().map(|s| s.to_string()).collect()];
     let input_ints: Vec<i32> = parse_csv_i32_lines(input_strings);
+
+    let value_a = puzzle_a(&input_ints);
+    println!("Solution to puzzle 1: {}", value_a);
 }
