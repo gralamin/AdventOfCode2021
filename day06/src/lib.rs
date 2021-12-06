2f7041775356b9acce4d2c2c28996c499affdcfd
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-05 22:58:18 -0700

    day06: Use much faster simulation for puzzle_b

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -39,6 +39,33 @@ fn simple_simulation(input: &Vec<i32>, num_cycle: usize) -> Vec<LanternFish> {
     return cur_cycle;
 }
 
+fn array_simulation(input: &Vec<i32>, num_cycle: usize) -> usize {
+    // instead of simulating each fish as an object, use a simple array of numbers
+    let mut fish: [usize; 7] = [0; 7];
+    let mut new_fish: [usize; 7] = [0; 7];
+    for x in input {
+        // initial seeding
+        let timer: usize = (*x).try_into().unwrap();
+        fish[timer] += 1;
+    }
+    for day in 0..num_cycle {
+        // the fish on this timer are going to be the ones that do anything
+        let num_fish_new = fish[day % 7];
+        // There are essentially two fields we need to worry about here
+        // (day + 2 ) % 7 is when these new fish should be considered, basically
+        // 2 days after this fish, this will be added to to the fish to worry about.
+        new_fish[(day + 2) % 7] = num_fish_new;
+        // Update the number of fish on the next iteration of this value, with
+        // thew number of new fish to care about.
+        fish[day % 7] += new_fish[day % 7];
+        // Clear out the number here to indicate we have used it.
+        new_fish[day % 7] = 0;
+    }
+    let mut num_fish: usize = fish.iter().sum::<usize>();
+    num_fish += new_fish.iter().sum::<usize>();
+    return num_fish;
+}
+
 /// Solution to the first puzzle.
 ///
 /// Likely this can be mathmatically modelled, but instead, going to create fish
@@ -52,6 +79,21 @@ pub fn puzzle_a(input: &Vec<i32>) -> usize {
     let total_cycles = 80;
     let finished_sim = simple_simulation(input, total_cycles);
     return finished_sim.len();
+}
+
+/// Solution to the second puzzle.
+///
+/// 256 is way too slow, runs for a long time just to do this test. As such
+/// we are switching to using an array simulation.
+///
+/// ```
+/// let input = vec![3, 4, 3, 1, 2];
+/// assert_eq!(day06::puzzle_b(&input), 26984457539);
+/// ```
+pub fn puzzle_b(input: &Vec<i32>) -> usize {
+    let total_cycles = 256;
+    let finished_sim = array_simulation(input, total_cycles);
+    return finished_sim;
 }
 
 #[cfg(test)]
@@ -83,5 +125,16 @@ mod tests {
         assert_eq!(simple_simulation(&start, 2).len(), 6);
         assert_eq!(simple_simulation(&start, 8).len(), 10);
         assert_eq!(simple_simulation(&start, 18).len(), 26);
+    }
+
+    #[test]
+    fn test_array_simulation() {
+        let start = vec![3, 4, 3, 1, 2];
+        assert_eq!(array_simulation(&start, 0), 5);
+        assert_eq!(array_simulation(&start, 1), 5);
+        assert_eq!(array_simulation(&start, 2), 6);
+        assert_eq!(array_simulation(&start, 8), 10);
+        assert_eq!(array_simulation(&start, 18), 26);
+        assert_eq!(array_simulation(&start, 80), 5934);
     }
 }
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,7 @@
 use day06::load;
 use day06::parse_csv_i32_lines;
 use day06::puzzle_a;
+use day06::puzzle_b;
 
 fn main() {
     let data = load("input");
@@ -9,4 +10,7 @@ fn main() {
 
     let value_a = puzzle_a(&input_ints);
     println!("Solution to puzzle 1: {}", value_a);
+
+    let value_b = puzzle_b(&input_ints);
+    println!("Solution to puzzle 2: {}", value_b);
 }
