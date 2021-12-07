26e511f24f69dbc027845058b77f2602dc64861e
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-06 22:36:19 -0700

    Day07: Solution part 1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,13 @@
+use day07::load;
+use day07::parse_csv_i32_lines;
+use day07::puzzle_a;
+//use day07::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let data = load("input");
+    let input_strings: Vec<Vec<String>> = vec![data.lines().map(|s| s.to_string()).collect()];
+    let input_ints: Vec<i32> = parse_csv_i32_lines(input_strings);
+
+    let value_a = puzzle_a(&input_ints);
+    println!("Solution to puzzle 1: {}", value_a);
 }
