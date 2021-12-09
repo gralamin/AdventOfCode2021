5cbbca2205b216280b574281b76aa2b16788e665
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-08 23:49:32 -0700

    Day09: Solution to part b

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -2,7 +2,7 @@ use day09::extract_all_nums;
 use day09::get_board_size;
 use day09::load;
 use day09::puzzle_a;
-//use day09::puzzle_b;
+use day09::puzzle_b;
 
 fn main() {
     let filename = "input";
@@ -11,12 +11,13 @@ fn main() {
     //assert_eq!(width, 100);
     //assert_eq!(height, 100);
     let numbers = extract_all_nums(&input);
+    let numbers_b = numbers.clone();
     let len: i32 = numbers.len().try_into().unwrap();
     assert_eq!(len, width * height);
 
     let value_a = puzzle_a(numbers, width, height);
     println!("Solution to 1: {}", value_a);
 
-    //let value_b = puzzle_b(&signals, &values);
-    // println!("Solution to 2: {}", value_b);
+    let value_b = puzzle_b(numbers_b, width, height);
+    println!("Solution to 2: {}", value_b);
 }
