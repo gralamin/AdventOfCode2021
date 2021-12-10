01da91ef25590b317187a76ff3baac0dedd703f8
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-09 22:51:54 -0700

    Day 10: solution to part 2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -15,13 +15,16 @@ enum Chunks {
 }
 
 impl Chunks {
-    fn get_points(&self) -> Option<u32> {
+    fn get_points(&self) -> Option<u64> {
         match self {
             Chunks::CloseRound => Some(3),
             Chunks::CloseSquare => Some(57),
             Chunks::CloseCurly => Some(1197),
             Chunks::CloseTriangle => Some(25137),
-            _ => None,
+            Chunks::OpenRound => Some(1),
+            Chunks::OpenSquare => Some(2),
+            Chunks::OpenCurly => Some(3),
+            Chunks::OpenTriangle => Some(4),
         }
     }
 
@@ -54,7 +57,7 @@ impl std::str::FromStr for Chunks {
     }
 }
 
-fn line_is_corrupted(line: &str) -> (bool, u32) {
+fn line_is_corrupted(line: &str) -> (bool, u64) {
     let mut unclosed_sets: Vec<Chunks> = Vec::new();
     let mut open_chunk_set: HashSet<Chunks> = HashSet::new();
     open_chunk_set.insert(Chunks::OpenRound);
@@ -77,13 +80,40 @@ fn line_is_corrupted(line: &str) -> (bool, u32) {
     return (false, 0);
 }
 
+fn complete_line(line: &str) -> u64 {
+    let mut unclosed_sets: Vec<Chunks> = Vec::new();
+    let mut open_chunk_set: HashSet<Chunks> = HashSet::new();
+    open_chunk_set.insert(Chunks::OpenRound);
+    open_chunk_set.insert(Chunks::OpenSquare);
+    open_chunk_set.insert(Chunks::OpenCurly);
+    open_chunk_set.insert(Chunks::OpenTriangle);
+
+    for x in line.chars() {
+        let cur_chunk: Chunks = x.to_string().parse::<Chunks>().unwrap();
+        if open_chunk_set.contains(&cur_chunk) {
+            unclosed_sets.push(cur_chunk);
+        } else {
+            // This isn't corrupt, so I can just pop it, its fine.
+            unclosed_sets.pop();
+        }
+    }
+
+    let mut score: u64 = 0;
+    while let Some(stack_value) = unclosed_sets.pop() {
+        score *= 5;
+        score += stack_value.get_points().unwrap();
+    }
+
+    return score;
+}
+
 /// Check if each line is corrupted, and add together thep oints of the ones that are corrupted
 ///
 /// ```
 /// let lines = "[({(<(())[]>[[{[]{<()<>>\n[(()[<>])]({[<{<<[]>>(\n{([(<{}[<>[]}>{[]{[(<()>\n(((({<>}<{<{<>}{[]{[]{}\n[[<[([]))<([[{}[[()]]]\n[{[{({}]{}}([{[{{{}}([]\n{<[[]]>}<{[{[{[]{()[[[]\n[<(<(<(<{}))><([]([]()\n<{([([[(<>()){}]>(<<{{\n<{([{{}}[<[[[<>{}]]]>[]]";
 /// assert_eq!(day10::puzzle_a(lines), 26397);
 /// ```
-pub fn puzzle_a(lines: &str) -> u32 {
+pub fn puzzle_a(lines: &str) -> u64 {
     let mut sum = 0;
     for line in lines.lines().map(|l| l.trim()).filter(|l| !l.is_empty()) {
         let ret_tuple = line_is_corrupted(line);
@@ -92,6 +122,24 @@ pub fn puzzle_a(lines: &str) -> u32 {
         }
     }
     return sum;
+}
+
+/// Check if each line is corrupted, complete the uncompleted one, and give the middle score
+///
+/// ```
+/// let lines = "[({(<(())[]>[[{[]{<()<>>\n[(()[<>])]({[<{<<[]>>(\n{([(<{}[<>[]}>{[]{[(<()>\n(((({<>}<{<{<>}{[]{[]{}\n[[<[([]))<([[{}[[()]]]\n[{[{({}]{}}([{[{{{}}([]\n{<[[]]>}<{[{[{[]{()[[[]\n[<(<(<(<{}))><([]([]()\n<{([([[(<>()){}]>(<<{{\n<{([{{}}[<[[[<>{}]]]>[]]";
+/// assert_eq!(day10::puzzle_b(lines), 288957);
+/// ```
+pub fn puzzle_b(lines: &str) -> u64 {
+    let incomplete_lines = lines
+        .lines()
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .filter(|x| !line_is_corrupted(x).0);
+    let mut results: Vec<u64> = incomplete_lines.map(|x| complete_line(x)).collect();
+    results.sort();
+    let middle = results.len() / 2;
+    return *results.iter().nth(middle).unwrap();
 }
 
 #[cfg(test)]
@@ -106,5 +154,14 @@ mod tests {
         assert_eq!(line_is_corrupted("[<(<(<(<{}))><([]([]()"), (true, 3));
         assert_eq!(line_is_corrupted("<{([([[(<>()){}]>(<<{{"), (true, 25137));
         assert_eq!(line_is_corrupted("<{([{{}}[<[[[<>{}]]]>[]]"), (false, 0));
+    }
+
+    #[test]
+    fn test_complete_line() {
+        assert_eq!(complete_line("[({(<(())[]>[[{[]{<()<>>"), 288957);
+        assert_eq!(complete_line("[(()[<>])]({[<{<<[]>>("), 5566);
+        assert_eq!(complete_line("(((({<>}<{<{<>}{[]{[]{}"), 1480781);
+        assert_eq!(complete_line("<[[]]>}<{[{[{[]{()[[[]"), 995444);
+        assert_eq!(complete_line("<{([{{}}[<[[[<>{}]]]>[]]"), 294);
     }
 }
--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,5 +1,6 @@
 use day10::load;
 use day10::puzzle_a;
+use day10::puzzle_b;
 
 fn main() {
     let filename = "input";
@@ -8,6 +9,6 @@ fn main() {
     let value_a = puzzle_a(&input);
     println!("Solution to 1: {}", value_a);
 
-    //let value_b = puzzle_b(numbers_b, width, height);
-    //println!("Solution to 2: {}", value_b);
+    let value_b = puzzle_b(&input);
+    println!("Solution to 2: {}", value_b);
 }
