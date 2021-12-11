8ca7c937dbf97db5155ea4571b97493725d8d2d0
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-10 22:43:00 -0700

    Day11: Solution to part a

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,13 @@
+use day11::extract_all_nums;
+use day11::load;
+use day11::puzzle_a;
+//use day11::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let input = load(filename);
+    let numbers = extract_all_nums(&input);
+
+    let value_a = puzzle_a(&numbers);
+    println!("Solution to 1: {}", value_a);
 }
