06c40723bfb25a5a3140e2b2fa23d00125520174
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-10 22:43:00 -0700

    Day11: Solution to part a

--- boardlib/src/lib.rs
+++ boardlib/src/lib.rs
@@ -97,18 +97,24 @@ pub enum Direction {
     EAST,
     SOUTH,
     WEST,
+    NORTHEAST,
+    SOUTHEAST,
+    SOUTHWEST,
+    NORTHWEST,
 }
 
 pub trait BoardTraversable {
     type Item;
 
     fn get_value(&self, pos: BoardCoordinate) -> Option<Self::Item>;
+    fn set_value(&mut self, pos: BoardCoordinate, value: Self::Item);
     fn get_coordinate_by_direction(
         &self,
         pos: BoardCoordinate,
         direction: Direction,
     ) -> Option<BoardCoordinate>;
     fn get_adjacent_coordinates(&self, pos: BoardCoordinate) -> Vec<BoardCoordinate>;
+    fn get_diag_adjacent_coordinates(&self, pos: BoardCoordinate) -> Vec<BoardCoordinate>;
 }
 
 impl<T: Copy> BoardTraversable for Board<T> {
@@ -123,6 +129,15 @@ impl<T: Copy> BoardTraversable for Board<T> {
         return Some(*(self.values.iter().nth(pos)?));
     }
 
+    fn set_value(&mut self, pos: BoardCoordinate, value: Self::Item) {
+        if pos.y >= self.height || pos.x >= self.width {
+            // y cannot exceed height, x cannot exceed width
+            return;
+        }
+        let pos: usize = pos.x + pos.y * self.width;
+        self.values[pos] = value;
+    }
+
     fn get_coordinate_by_direction(
         &self,
         pos: BoardCoordinate,
@@ -135,6 +150,22 @@ impl<T: Copy> BoardTraversable for Board<T> {
             Direction::EAST => possible_x = pos.x.checked_add(1),
             Direction::SOUTH => possible_y = pos.y.checked_add(1),
             Direction::WEST => possible_x = pos.x.checked_sub(1),
+            Direction::NORTHEAST => {
+                possible_x = pos.x.checked_add(1);
+                possible_y = pos.y.checked_sub(1);
+            }
+            Direction::SOUTHEAST => {
+                possible_x = pos.x.checked_add(1);
+                possible_y = pos.y.checked_add(1);
+            }
+            Direction::SOUTHWEST => {
+                possible_x = pos.x.checked_sub(1);
+                possible_y = pos.y.checked_add(1);
+            }
+            Direction::NORTHWEST => {
+                possible_x = pos.x.checked_sub(1);
+                possible_y = pos.y.checked_sub(1);
+            }
         }
         if let Some(new_x) = possible_x {
             if let Some(new_y) = possible_y {
@@ -154,6 +185,28 @@ impl<T: Copy> BoardTraversable for Board<T> {
         let opt_west = self.get_coordinate_by_direction(pos, Direction::WEST);
         let mut result: Vec<BoardCoordinate> = Vec::new();
         let options = vec![opt_north, opt_east, opt_south, opt_west];
+
+        for possible_pos in options {
+            if let Some(cur_pos) = possible_pos {
+                result.push(cur_pos);
+            }
+        }
+
+        return result;
+    }
+
+    fn get_diag_adjacent_coordinates(&self, pos: BoardCoordinate) -> Vec<BoardCoordinate> {
+        let opt_north_east = self.get_coordinate_by_direction(pos, Direction::NORTHEAST);
+        let opt_south_east = self.get_coordinate_by_direction(pos, Direction::SOUTHEAST);
+        let opt_south_west = self.get_coordinate_by_direction(pos, Direction::SOUTHWEST);
+        let opt_north_west = self.get_coordinate_by_direction(pos, Direction::NORTHWEST);
+        let mut result: Vec<BoardCoordinate> = Vec::new();
+        let options = vec![
+            opt_north_east,
+            opt_south_east,
+            opt_south_west,
+            opt_north_west,
+        ];
 
         for possible_pos in options {
             if let Some(cur_pos) = possible_pos {
@@ -190,6 +243,15 @@ mod tests {
     }
 
     #[test]
+    fn test_set_board_number() {
+        let mut board = produce_board();
+        let coord = BoardCoordinate::new(3, 3);
+        assert_eq!(board.get_value(coord), Some(7));
+        board.set_value(coord, 99);
+        assert_eq!(board.get_value(coord), Some(99));
+    }
+
+    #[test]
     fn test_get_adjacent_coordinates() {
         let board = produce_board();
         assert_eq!(
@@ -207,6 +269,27 @@ mod tests {
         assert_eq!(
             board.get_adjacent_coordinates(BoardCoordinate::new(9, 4)),
             vec![BoardCoordinate::new(9, 3), BoardCoordinate::new(8, 4)]
+        );
+    }
+
+    #[test]
+    fn test_get_diag_adjacent_coordinates() {
+        let board = produce_board();
+        assert_eq!(
+            board.get_diag_adjacent_coordinates(BoardCoordinate::new(0, 0)),
+            vec![BoardCoordinate::new(1, 1)]
+        );
+        assert_eq!(
+            board.get_diag_adjacent_coordinates(BoardCoordinate::new(9, 0)),
+            vec![BoardCoordinate::new(8, 1)]
+        );
+        assert_eq!(
+            board.get_diag_adjacent_coordinates(BoardCoordinate::new(0, 4)),
+            vec![BoardCoordinate::new(1, 3)]
+        );
+        assert_eq!(
+            board.get_diag_adjacent_coordinates(BoardCoordinate::new(9, 4)),
+            vec![BoardCoordinate::new(8, 3)]
         );
     }
 
