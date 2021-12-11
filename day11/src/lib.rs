794d5006daa8d99343025569a22034573b52b2ed
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-10 22:47:41 -0700

    Day 11: Solution part 2

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -100,6 +100,28 @@ pub fn puzzle_a(values: &Vec<u32>) -> u32 {
     return flashes;
 }
 
+/// Find the first step on which the octopuses sync (100 flashes)
+///
+/// ```
+/// let v = vec![
+///     5, 4, 8, 3, 1, 4, 3, 2, 2, 3, 2, 7, 4, 5, 8, 5, 4, 7, 1, 1, 5, 2, 6, 4, 5, 5, 6, 1, 7,
+///     3, 6, 1, 4, 1, 3, 3, 6, 1, 4, 6, 6, 3, 5, 7, 3, 8, 5, 4, 7, 8, 4, 1, 6, 7, 5, 2, 4, 6,
+///     4, 5, 2, 1, 7, 6, 8, 4, 1, 7, 2, 1, 6, 8, 8, 2, 8, 8, 1, 1, 3, 4, 4, 8, 4, 6, 8, 4, 8,
+///     5, 5, 4, 5, 2, 8, 3, 7, 5, 1, 5, 2, 6,
+/// ];
+/// assert_eq!(day11::puzzle_b(&v), 195);
+/// ```
+pub fn puzzle_b(values: &Vec<u32>) -> u32 {
+    let mut board = OctopusBoard::new(values.to_vec());
+    let mut flashes: u32 = 0;
+    let mut step: u32 = 0;
+    while flashes != 100 {
+        step += 1;
+        flashes = board.take_a_step();
+    }
+    return step;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,7 +1,7 @@
 use day11::extract_all_nums;
 use day11::load;
 use day11::puzzle_a;
-//use day11::puzzle_b;
+use day11::puzzle_b;
 
 fn main() {
     let filename = "input";
@@ -10,4 +10,7 @@ fn main() {
 
     let value_a = puzzle_a(&numbers);
     println!("Solution to 1: {}", value_a);
+
+    let value_b = puzzle_b(&numbers);
+    println!("Solution to 2: {}", value_b);
 }
