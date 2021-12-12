5051bb26bc52218ebb5b3a3e20332a8133bcfdcf
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-12 00:24:35 -0700

    day12: solution to part 1

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,14 @@
+use day12::load_no_blanks;
+use day12::puzzle_a;
+//use day12::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load_no_blanks(filename);
+
+    let value_a = puzzle_a(&all_lines);
+    println!("Solution to 1: {}", value_a);
+
+    //let value_b = puzzle_b(&all_lines);
+    //println!("Solution to 2: {}", value_b);
 }
