1b0e9fc2753463acbf86b55cb107a93e83188557
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-12 23:22:45 -0700

    Day13: Solution to part 1

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,3 +1,30 @@
+use day13::load;
+use day13::parse_coords;
+use day13::parse_folds;
+use day13::puzzle_a;
+use day13::split_lines_by_blanks;
+use day13::Fold;
+use day13::FxHashSet;
+//use day13::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+    let sections_split = split_lines_by_blanks(&all_lines);
+    let mut coords = FxHashSet::default();
+    let mut folds: Vec<Fold> = Vec::new();
+
+    if let Some(coord_section) = sections_split.first() {
+        coords = parse_coords(coord_section);
+    }
+
+    if let Some(fold_section) = sections_split.last() {
+        folds = parse_folds(fold_section);
+    }
+
+    let value_a = puzzle_a(&coords, &folds);
+    println!("Solution to 1: {}", value_a);
+
+    //let value_b = puzzle_b(&coords, &folds);
+    //println!("Solution to 2: {}", value_b);
 }
