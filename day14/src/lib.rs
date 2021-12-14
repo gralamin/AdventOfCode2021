d0844602a05329c50b572f2f1e5582769628ac99
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-14 00:28:32 -0700

    Day14: Huge refactor to solution to part 2
    Instead of actually making the strings, just count the pairs we should have.

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -23,67 +23,6 @@ impl PolyPair {
     }
 }
 
-#[derive(Debug)]
-struct Polymer {
-    data: String,
-}
-
-impl Polymer {
-    fn new(data: String) -> Polymer {
-        return Polymer { data: data };
-    }
-
-    fn get_pair_iter(&self) -> PairIter {
-        return PairIter {
-            base_data: self,
-            cur_index: 0,
-        };
-    }
-}
-
-#[derive(Debug)]
-struct PairIter<'a> {
-    base_data: &'a Polymer,
-    cur_index: usize,
-}
-
-impl<'a> Iterator for PairIter<'a> {
-    type Item = PolyPair;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.cur_index + 1 >= self.base_data.data.len() {
-            return None;
-        }
-        let a = &self.base_data.data[self.cur_index..self.cur_index + 1];
-        let b = &self.base_data.data[self.cur_index + 1..self.cur_index + 2];
-        self.cur_index += 1;
-        return Some(Self::Item::from(a, b));
-    }
-}
-
-fn polymer_cycle(polymer: &Polymer, rules: &FxHashMap<PolyPair, String>) -> Polymer {
-    let mut new_data: Vec<String> = Vec::new();
-
-    let mut first = true;
-    for pair in polymer.get_pair_iter() {
-        if let Some(x) = rules.get(&pair) {
-            if first {
-                new_data.push(pair.a);
-            }
-            new_data.push(x.to_string());
-            new_data.push(pair.b);
-        } else {
-            if first {
-                new_data.push(pair.a);
-            }
-            new_data.push(pair.b);
-        }
-        first = false;
-    }
-
-    return Polymer::new(new_data.join(""));
-}
-
 /// Parse a set of rules
 ///
 /// ```
@@ -106,38 +45,6 @@ pub fn create_rules(inputs: Vec<String>) -> FxHashMap<PolyPair, String> {
     return map;
 }
 
-/// Get the complete set of characters that appears
-///
-/// ```
-/// use rustc_hash::{FxHashMap, FxHashSet};
-/// use day14::PolyPair;
-/// let mut rules: FxHashMap<PolyPair, String> = FxHashMap::default();
-/// rules.insert(PolyPair::new("CH"), "B".to_string());
-/// rules.insert(PolyPair::new("HH"), "N".to_string());
-/// let template = "NNCB";
-/// let mut alphabet = FxHashSet::default();
-/// alphabet.insert("N".to_string());
-/// alphabet.insert("C".to_string());
-/// alphabet.insert("B".to_string());
-/// alphabet.insert("H".to_string());
-/// assert_eq!(day14::get_alphabet(template, &rules), alphabet)
-/// ```
-pub fn get_alphabet(
-    polymer_template: &str,
-    rules: &FxHashMap<PolyPair, String>,
-) -> FxHashSet<String> {
-    let mut alphabet = FxHashSet::default();
-    for c in polymer_template.chars() {
-        alphabet.insert(c.to_string());
-    }
-    for (rule, value) in rules {
-        alphabet.insert(rule.a.clone());
-        alphabet.insert(rule.b.clone());
-        alphabet.insert(value.to_string());
-    }
-    return alphabet;
-}
-
 /// Cycle the polymer 10 times, then get the top two character counts, and subtract from each other
 ///
 /// ```
@@ -161,38 +68,55 @@ pub fn get_alphabet(
 /// rules.insert(PolyPair::new("CC"), "N".to_string());
 /// rules.insert(PolyPair::new("CN"), "C".to_string());
 /// let template = "NNCB";
-/// let alphabet = day14::get_alphabet(template, &rules);
-/// assert_eq!(day14::puzzle_a(template, &rules, &alphabet), 1588);
+/// assert_eq!(day14::puzzle_a(template, &rules), 1588);
 /// ```
-pub fn puzzle_a(
-    polymer_template: &str,
-    rules: &FxHashMap<PolyPair, String>,
-    alphabet: &FxHashSet<String>,
-) -> usize {
-    return common_puzzle(polymer_template, rules, alphabet, 10);
+pub fn puzzle_a(polymer_template: &str, rules: &FxHashMap<PolyPair, String>) -> usize {
+    return common_puzzle(polymer_template, rules, 10);
 }
 
-fn common_puzzle(polymer_template: &str,
+/// Takes too long to actually do it, so use counting instead.
+fn common_puzzle(
+    polymer_template: &str,
     rules: &FxHashMap<PolyPair, String>,
-    alphabet: &FxHashSet<String>,
-num_runs: usize) -> usize {
-    let mut polymer = Polymer::new(polymer_template.to_string());
+    num_runs: usize,
+) -> usize {
+    let mut counts = FxHashMap::default();
+    for i in polymer_template.chars().collect::<Vec<_>>().windows(2) {
+        // Create pairs for everything in the template at 1.
+        let pair = PolyPair::from(&i[0].to_string(), &i[1].to_string());
+        *counts.entry(pair).or_default() += 1;
+    }
+
     for _ in 0..num_runs {
-        polymer = polymer_cycle(&polymer, &rules);
+        let mut current = FxHashMap::default();
+        for (key, c) in counts.iter() {
+            // Have a matching rule, so increase counts of results of inserting.
+            if let Some(letter) = rules.get(key) {
+                let new_pair_left = PolyPair::from(&key.a, &letter);
+                let new_pair_right = PolyPair::from(&letter, &key.b);
+                *current.entry(new_pair_left).or_default() += c;
+                *current.entry(new_pair_right).or_default() += c;
+            }
+        }
+        counts = current;
     }
 
-    let mut top_count = 0;
-    let mut bottom_count = polymer.data.len();
-    for character in alphabet {
-        let count = polymer.data.matches(character).count();
-        if count > top_count {
-            top_count = count;
-        }
-        if count < bottom_count {
-            bottom_count = count;
-        }
-    }
-    return top_count - bottom_count;
+    // We have all the counts, extract the max and min. Turns out, fold can help us do this.
+    // We basically count all the occurances of the second characters we have.
+    let mut final_counts = counts.iter().fold(
+        FxHashMap::<String, usize>::default(),
+        |mut m: FxHashMap<String, usize>, (k, v): (&PolyPair, &usize)| {
+            *m.entry(k.b.clone()).or_default() += v;
+            return m;
+        },
+    );
+    // Only thing that will be missing is first character.
+    let first_char: String = polymer_template.chars().nth(0).unwrap().to_string();
+    *final_counts.entry(first_char).or_default() += 1;
+    // Magicy methods to get max and min easy :O
+    let max = final_counts.iter().max_by_key(|(_, i)| *i).unwrap().1;
+    let min = final_counts.iter().min_by_key(|(_, i)| *i).unwrap().1;
+    return max - min;
 }
 
 /// Cycle the polymer 10 times, then get the top two character counts, and subtract from each other
@@ -218,76 +142,8 @@ num_runs: usize) -> usize {
 /// rules.insert(PolyPair::new("CC"), "N".to_string());
 /// rules.insert(PolyPair::new("CN"), "C".to_string());
 /// let template = "NNCB";
-/// let alphabet = day14::get_alphabet(template, &rules);
-/// assert_eq!(day14::puzzle_b(template, &rules, &alphabet), 2188189693529);
+/// assert_eq!(day14::puzzle_b(template, &rules), 2188189693529);
 /// ```
-pub fn puzzle_b(
-    polymer_template: &str,
-    rules: &FxHashMap<PolyPair, String>,
-    alphabet: &FxHashSet<String>,
-) -> usize {
-    return common_puzzle(polymer_template, rules, alphabet, 40);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    fn make_polymer() -> Polymer {
-        let input = "NNCB".to_string();
-        return Polymer::new(input);
-    }
-
-    fn make_rules() -> FxHashMap<PolyPair, String> {
-        let mut map = FxHashMap::default();
-        map.insert(PolyPair::new("CH"), "B".to_string());
-        map.insert(PolyPair::new("HH"), "N".to_string());
-        map.insert(PolyPair::new("CB"), "H".to_string());
-        map.insert(PolyPair::new("NH"), "C".to_string());
-        map.insert(PolyPair::new("HB"), "C".to_string());
-        map.insert(PolyPair::new("HC"), "B".to_string());
-        map.insert(PolyPair::new("HN"), "C".to_string());
-        map.insert(PolyPair::new("NN"), "C".to_string());
-        map.insert(PolyPair::new("BH"), "H".to_string());
-        map.insert(PolyPair::new("NC"), "B".to_string());
-        map.insert(PolyPair::new("NB"), "B".to_string());
-        map.insert(PolyPair::new("BN"), "B".to_string());
-        map.insert(PolyPair::new("BB"), "N".to_string());
-        map.insert(PolyPair::new("BC"), "B".to_string());
-        map.insert(PolyPair::new("CC"), "N".to_string());
-        map.insert(PolyPair::new("CN"), "C".to_string());
-        return map;
-    }
-
-    #[test]
-    fn test_polymer_get_pairs() {
-        let polymer = make_polymer();
-        let result: Vec<PolyPair> = polymer.get_pair_iter().collect();
-        assert_eq!(result.len(), 3);
-        assert_eq!(
-            result,
-            vec![
-                PolyPair::new("NN"),
-                PolyPair::new("NC"),
-                PolyPair::new("CB")
-            ]
-        );
-    }
-
-    #[test]
-    fn test_polymer_cycle() {
-        let rules = make_rules();
-        let polymer = make_polymer();
-        let result = polymer_cycle(&polymer, &rules);
-        assert_eq!(result.data, "NCNBCHB");
-        let result_2 = polymer_cycle(&result, &rules);
-        assert_eq!(result_2.data, "NBCCNBBBCBHCB");
-        let result_3 = polymer_cycle(&result_2, &rules);
-        assert_eq!(result_3.data, "NBBBCNCCNBBNBNBBCHBHHBCHB");
-        let result_4 = polymer_cycle(&result_3, &rules);
-        assert_eq!(
-            result_4.data,
-            "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
-        );
-    }
+pub fn puzzle_b(polymer_template: &str, rules: &FxHashMap<PolyPair, String>) -> usize {
+    return common_puzzle(polymer_template, rules, 40);
 }
