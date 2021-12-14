46435fbc30a9d0241794d167e9b4a8f82d900ae7
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-14 00:28:32 -0700

    Day14: Huge refactor to solution to part 2
    Instead of actually making the strings, just count the pairs we should have.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,8 +1,7 @@
 use day14::puzzle_a;
-use day14::{load, split_lines_by_blanks};
 use day14::puzzle_b;
 use day14::{create_rules, FxHashMap, PolyPair};
-use day14::{get_alphabet, FxHashSet};
+use day14::{load, split_lines_by_blanks};
 
 fn main() {
     let filename = "input";
@@ -10,7 +9,6 @@ fn main() {
     let sections_split = split_lines_by_blanks(&all_lines);
     let template: String;
     let rules: FxHashMap<PolyPair, String>;
-    let alphabet: FxHashSet<String>;
 
     let template_section = sections_split.first().unwrap();
     template = template_section.first().unwrap().to_string();
@@ -18,11 +16,9 @@ fn main() {
     let rules_section = sections_split.last().unwrap();
     rules = create_rules(rules_section.to_vec());
 
-    alphabet = get_alphabet(&template, &rules);
-
-    let value_a = puzzle_a(&template, &rules, &alphabet);
+    let value_a = puzzle_a(&template, &rules);
     println!("Solution to 1: {}", value_a);
 
-    let value_b = puzzle_b(&template, &rules, &alphabet);
+    let value_b = puzzle_b(&template, &rules);
     println!("Solution to 2: {}", value_b);
 }
