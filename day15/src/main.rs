6df3b308fdb204eb18c770375b64d805ce770d94
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-14 23:16:40 -0700

    Day15: Solution to part 1 via Dijkstra

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,14 @@
+use day15::load;
+use day15::puzzle_a;
+//use day15::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+
+    let value_a = puzzle_a(all_lines);
+    println!("Solution to 1: {}", value_a);
+
+    //let value_b = puzzle_b(all_lines);
+    //println!("Solution to 2: {}", value_b);
 }
