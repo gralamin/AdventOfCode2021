d975be972e2c6f16484e84f9962cdd4f4214c045
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-16 23:32:32 -0700

    Day 17: Part 1 solution

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,3 +1,16 @@
+use day17::load;
+use day17::load_target_area;
+use day17::puzzle_a;
+//use day17::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+    let (x1, y1, x2, y2) = load_target_area(&all_lines);
+
+    let value_a = puzzle_a(x1, y1, x2, y2);
+    println!("Solution to 1: {}", value_a);
+
+    //let value_b = puzzle_b(&packet_stream);
+    //println!("Solution to 2: {}", value_b);
 }
