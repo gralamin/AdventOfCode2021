947800c45daed231cc49705f37c771e11479ee61
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-18 11:31:34 -0700

    Day18: Solution to part 1
    This was stolen from this reddit post https://www.reddit.com/r/adventofcode/comments/rizw2c/comment/hp1lx2j/?utm_source=share&utm_medium=web2x&context=3
    I failed at this repreatedly, not because of the difficulty of the assignment,
    but because my rust knowledge isn't good enough to get this working well.

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,3 +1,16 @@
+use day18::load;
+use day18::parse;
+use day18::puzzle_a;
+//use day18::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+    let parsed = parse(&all_lines);
+    
+    let value_a = puzzle_a(&parsed);
+    println!("Solution to 1: {}", value_a);
+
+    // let value_b = puzzle_b(&parsed);
+    // println!("Solution to 2: {}", value_b);
 }
