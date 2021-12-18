9bea42c14604b17db0e5936880c87b91114fa81a
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-18 11:31:34 -0700

    Day18: Solution to part 1
    This was stolen from this reddit post https://www.reddit.com/r/adventofcode/comments/rizw2c/comment/hp1lx2j/?utm_source=share&utm_medium=web2x&context=3
    I failed at this repreatedly, not because of the difficulty of the assignment,
    but because my rust knowledge isn't good enough to get this working well.

---
+++ day18/src/lib.rs
@@ -0,0 +1,157 @@
+pub use filelib::load;
+
+#[derive(Clone)]
+pub enum SnailNumber {
+    Num(u8),
+    Pair(Box<SnailNumber>, Box<SnailNumber>),
+}
+
+fn read_snail_num(text: &str) -> (Box<SnailNumber>, usize) {
+    if text[0..1] == *"[" {
+        let (left, left_id) = read_snail_num(&text[1..]);
+        assert_eq!(text[left_id + 1..left_id + 2], *",");
+        let (right, right_id) = read_snail_num(&text[(left_id + 2)..]);
+        assert_eq!(text[left_id + right_id + 2..left_id + right_id + 3], *"]");
+        (
+            Box::new(SnailNumber::Pair(left, right)),
+            left_id + right_id + 3,
+        )
+    } else {
+        // Must be a number.
+        (
+            Box::new(SnailNumber::Num(text[0..1].parse::<u8>().unwrap())),
+            1,
+        )
+    }
+}
+
+pub fn parse(input: &str) -> Vec<Box<SnailNumber>> {
+    input
+        .lines()
+        .map(|x| x.trim())
+        .filter(|x| !x.is_empty())
+        .map(|x| read_snail_num(x).0)
+        .collect()
+}
+
+fn split(snail: &mut Box<SnailNumber>) -> bool {
+    match snail.as_mut() {
+        SnailNumber::Num(n) => {
+            if *n >= 10 {
+                **snail = SnailNumber::Pair(
+                    Box::new(SnailNumber::Num(*n / 2)),
+                    Box::new(SnailNumber::Num((*n + 1) / 2)),
+                );
+                true
+            } else {
+                false
+            }
+        }
+        SnailNumber::Pair(l, r) => {
+            let mut ok = split(l);
+            if !ok {
+                ok = split(r);
+            }
+
+            ok
+        }
+    }
+}
+
+fn add_to_right(snail: &mut Box<SnailNumber>, val: u8) {
+    match snail.as_mut() {
+        SnailNumber::Num(n) => *n += val,
+        SnailNumber::Pair(_, r) => add_to_right(r, val),
+    }
+}
+
+fn add_to_left(snail: &mut Box<SnailNumber>, val: u8) {
+    match snail.as_mut() {
+        SnailNumber::Num(n) => *n += val,
+        SnailNumber::Pair(l, _) => add_to_left(l, val),
+    }
+}
+
+fn explode(snail: &mut Box<SnailNumber>, depth: u8) -> (bool, Option<u8>, Option<u8>) {
+    match snail.as_mut() {
+        SnailNumber::Num(_) => (false, None, None),
+        SnailNumber::Pair(l, r) => {
+            if depth == 4 {
+                match (&**l, &**r) {
+                    (SnailNumber::Num(left), SnailNumber::Num(right)) => {
+                        let ret = (true, Some(*left), Some(*right));
+                        **snail = SnailNumber::Num(0);
+                        ret
+                    }
+                    (_, _) => panic!("Tree too deep already!"),
+                }
+            } else {
+                let left = explode(l, depth + 1);
+                if left.0 {
+                    if let Some(val) = left.2 {
+                        add_to_left(r, val);
+                        (true, left.1, None)
+                    } else {
+                        left
+                    }
+                } else {
+                    let right = explode(r, depth + 1);
+                    if right.0 {
+                        if let Some(val) = right.1 {
+                            add_to_right(l, val);
+                            (true, None, right.2)
+                        } else {
+                            right
+                        }
+                    } else {
+                        (false, None, None)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn calc_magnitude(snail: &SnailNumber) -> u64 {
+    match snail {
+        SnailNumber::Num(n) => *n as u64,
+        SnailNumber::Pair(l, r) => 3 * calc_magnitude(l) + 2 * calc_magnitude(r),
+    }
+}
+
+fn reduce(snail: &mut Box<SnailNumber>) {
+    loop {
+        let x = explode(snail, 0);
+        if !x.0 {
+            let x = split(snail);
+            if !x {
+                break;
+            }
+        }
+    }
+}
+
+pub fn puzzle_a(v: &[Box<SnailNumber>]) -> u64 {
+    let mut sum = v[0].clone();
+    for snail in v[1..].iter() {
+        sum = Box::new(SnailNumber::Pair(sum, snail.clone()));
+        reduce(&mut sum);
+    }
+    calc_magnitude(&sum)
+}
+
+
+fn print_snail(snail: &SnailNumber) {
+    match snail {
+        SnailNumber::Num(n) => {
+            print!("{}", *n)
+        }
+        SnailNumber::Pair(l, r) => {
+            print!("[");
+            print_snail(l);
+            print!(",");
+            print_snail(r);
+            print!("]");
+        }
+    }
+}
