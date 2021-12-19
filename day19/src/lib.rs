bf880cb3a687209c618aa741f4e9f536b162d2a3
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-19 15:40:28 -0700

    day19: Solution to part 2

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -1,6 +1,7 @@
 pub use filelib::{load, split_lines_by_blanks};
 use itertools::Itertools;
 pub use rustc_hash::{FxHashMap, FxHashSet};
+use std::cmp::max;
 use std::convert::Infallible;
 use std::fmt::{Debug, Formatter};
 use std::hash::{Hash, Hasher};
@@ -48,6 +49,16 @@ impl IVec3 {
             y: -self.x,
             z: self.z,
         };
+    }
+
+    fn dist_to(&self, other: &IVec3) -> u32 {
+        return (*self - *other).dist();
+    }
+
+    fn dist(&self) -> u32 {
+        return (self.x.abs() + self.y.abs() + self.z.abs())
+            .try_into()
+            .unwrap();
     }
 }
 
@@ -171,7 +182,7 @@ pub fn puzzle_a(scanner_map: &FxHashMap<usize, Vec<IVec3>>) -> usize {
         .map(|(_, points)| chiral_octahedral_symmetry(points.to_vec()))
         .collect();
 
-    let map_of_beacons: Vec<_> = build_beacon_map(cache);
+    let (map_of_beacons, _) = build_beacon_map(cache);
 
     return map_of_beacons.len();
 }
@@ -185,7 +196,7 @@ pub fn puzzle_a(scanner_map: &FxHashMap<usize, Vec<IVec3>>) -> usize {
 // To check if points match, we are going to choose a beacon in the left of the pair,
 // and translate each point on the right pair such that it matches. Then check if we have 12 matches.
 // If we do have 12 matches, thats it! If we don't choose the next point on the left.
-fn build_beacon_map(mut cache: Vec<Vec<Vec<IVec3>>>) -> Vec<IVec3> {
+fn build_beacon_map(mut cache: Vec<Vec<Vec<IVec3>>>) -> (Vec<IVec3>, Vec<IVec3>) {
     // Cache structure:
     // first vec is each scanner
     // second vec is each possible orientation of that scanner
@@ -194,14 +205,16 @@ fn build_beacon_map(mut cache: Vec<Vec<Vec<IVec3>>>) -> Vec<IVec3> {
     // We decide first scanner is current origin.
     let mut beacons_at_origin = cache[0][0].clone();
     cache.remove(0);
+    let mut scanners = vec![];
     while cache.len() > 0 {
         // We have ideas of how to optimize right now, but just brute force and see how slow it is.
         let mut used = cache.len();
         for (i, point_orientations) in cache.iter().enumerate() {
-            if let Some(result) =
+            if let Some((scanner_location, result)) =
                 find_overlap_with(beacons_at_origin.clone(), point_orientations.to_vec())
             {
                 beacons_at_origin = result;
+                scanners.push(scanner_location);
                 used = i;
                 break;
             }
@@ -212,26 +225,30 @@ fn build_beacon_map(mut cache: Vec<Vec<Vec<IVec3>>>) -> Vec<IVec3> {
         }
     }
 
-    return beacons_at_origin;
+    return (beacons_at_origin, scanners);
 }
 
 // Return the overlapped values all at origin 0 (beacons_at_origin's, original location)
 fn find_overlap_with(
     beacons_at_origin: Vec<IVec3>,
     point_orientations: Vec<Vec<IVec3>>,
-) -> Option<Vec<IVec3>> {
+) -> Option<(IVec3, Vec<IVec3>)> {
     let mut result: Vec<IVec3> = Vec::new();
     for orientation in point_orientations.iter() {
         for i in 0..beacons_at_origin.len() {
             let (origin, beacons_at_nth) = relative_to_nth(&beacons_at_origin, i);
             for j in 0..orientation.len() {
-                let (_j_translation, orientation_over_beacon) = relative_to_nth(&orientation, j);
+                let (j_translation, orientation_over_beacon) = relative_to_nth(&orientation, j);
                 if let Some(points) = overlap(&beacons_at_nth, &orientation_over_beacon) {
                     result.extend(beacons_at_nth);
                     result.extend(points);
-                    let map_back = |v: &IVec3| *v + origin;
+                    let map_back = |v: &IVec3| *v - origin;
                     result = result.iter().map(map_back).collect();
-                    return Some(result);
+                    // scanner location should be: Becaon location, which is 0,0,0 to beacon
+                    // Then subtract the j_translation, which should be the offset between the beacon and the scanner
+                    // so that should be 0,0,0 to scanner.
+                    let scanner_location: IVec3 = origin - j_translation;
+                    return Some((scanner_location, result));
                 }
             }
         }
@@ -260,6 +277,27 @@ fn overlap(lefts_points: &FxHashSet<IVec3>, right_points: &FxHashSet<IVec3>) -> 
         return None;
     }
     return Some(new_values);
+}
+
+/// Find the largest distance between points
+///
+/// Test for this elsewhere, because this is so complicated of an input.
+pub fn puzzle_b(scanner_map: &FxHashMap<usize, Vec<IVec3>>) -> u32 {
+    // Cache all the possible orientations
+    let cache: Vec<Vec<Vec<IVec3>>> = scanner_map
+        .iter()
+        .map(|(_, points)| chiral_octahedral_symmetry(points.to_vec()))
+        .collect();
+
+    let (_, map_of_scanners) = build_beacon_map(cache);
+    let mut largest: u32 = 0;
+    for (i, x) in map_of_scanners.iter().enumerate() {
+        for y in map_of_scanners.iter().skip(i + 1) {
+            largest = max(largest, x.dist_to(y));
+        }
+    }
+
+    return largest;
 }
 
 #[cfg(test)]
@@ -340,9 +378,21 @@ mod tests {
     fn test_puzzle_a() {
         let input = "--- scanner 0 ---\n404,-588,-901\n528,-643,409\n-838,591,734\n390,-675,-793\n-537,-823,-458\n-485,-357,347\n-345,-311,381\n-661,-816,-575\n-876,649,763\n-618,-824,-621\n553,345,-567\n474,580,667\n-447,-329,318\n-584,868,-557\n544,-627,-890\n564,392,-477\n455,729,728\n-892,524,684\n-689,845,-530\n423,-701,434\n7,-33,-71\n630,319,-379\n443,580,662\n-789,900,-551\n459,-707,401\n\n--- scanner 1 ---\n686,422,578\n605,423,415\n515,917,-361\n-336,658,858\n95,138,22\n-476,619,847\n-340,-569,-846\n567,-361,727\n-460,603,-452\n669,-402,600\n729,430,532\n-500,-761,534\n-322,571,750\n-466,-666,-811\n-429,-592,574\n-355,545,-477\n703,-491,-529\n-328,-685,520\n413,935,-424\n-391,539,-444\n586,-435,557\n-364,-763,-893\n807,-499,-711\n755,-354,-619\n553,889,-390\n\n--- scanner 2 ---\n649,640,665\n682,-795,504\n-784,533,-524\n-644,584,-595\n-588,-843,648\n-30,6,44\n-674,560,763\n500,723,-460\n609,671,-379\n-555,-800,653\n-675,-892,-343\n697,-426,-610\n578,704,681\n493,664,-388\n-671,-858,530\n-667,343,800\n571,-461,-707\n-138,-166,112\n-889,563,-600\n646,-828,498\n640,759,510\n-630,509,768\n-681,-892,-333\n673,-379,-804\n-742,-814,-386\n577,-820,562\n\n--- scanner 3 ---\n-589,542,597\n605,-692,669\n-500,565,-823\n-660,373,557\n-458,-679,-417\n-488,449,543\n-626,468,-788\n338,-750,-386\n528,-832,-391\n562,-778,733\n-938,-730,414\n543,643,-506\n-524,371,-870\n407,773,750\n-104,29,83\n378,-903,-323\n-778,-728,485\n426,699,580\n-438,-605,-362\n-469,-447,-387\n509,732,623\n647,635,-688\n-868,-804,481\n614,-800,639\n595,780,-596\n\n--- scanner 4 ---\n727,592,562\n-293,-554,779\n441,611,-461\n-714,465,-776\n-743,427,-804\n-660,-479,-426\n832,-632,460\n927,-485,-438\n408,393,-506\n466,436,-512\n110,16,151\n-258,-428,682\n-393,719,612\n-211,-452,876\n808,-476,-593\n-575,615,604\n-485,667,467\n-680,325,-822\n-627,-443,-432\n872,-547,-609\n833,512,582\n807,604,487\n839,-516,451\n891,-625,532\n-652,-548,-490\n30,-46,-14";
         let split = split_lines_by_blanks(&input);
-        let scanner_map: FxHashMap<usize, Vec<IVec3>> = split.iter().map(|x| parse_scanner(x)).collect();
+        let scanner_map: FxHashMap<usize, Vec<IVec3>> =
+            split.iter().map(|x| parse_scanner(x)).collect();
 
         let result = puzzle_a(&scanner_map);
         assert_eq!(result, 79);
+    }
+
+    #[test]
+    fn test_puzzle_b() {
+        let input = "--- scanner 0 ---\n404,-588,-901\n528,-643,409\n-838,591,734\n390,-675,-793\n-537,-823,-458\n-485,-357,347\n-345,-311,381\n-661,-816,-575\n-876,649,763\n-618,-824,-621\n553,345,-567\n474,580,667\n-447,-329,318\n-584,868,-557\n544,-627,-890\n564,392,-477\n455,729,728\n-892,524,684\n-689,845,-530\n423,-701,434\n7,-33,-71\n630,319,-379\n443,580,662\n-789,900,-551\n459,-707,401\n\n--- scanner 1 ---\n686,422,578\n605,423,415\n515,917,-361\n-336,658,858\n95,138,22\n-476,619,847\n-340,-569,-846\n567,-361,727\n-460,603,-452\n669,-402,600\n729,430,532\n-500,-761,534\n-322,571,750\n-466,-666,-811\n-429,-592,574\n-355,545,-477\n703,-491,-529\n-328,-685,520\n413,935,-424\n-391,539,-444\n586,-435,557\n-364,-763,-893\n807,-499,-711\n755,-354,-619\n553,889,-390\n\n--- scanner 2 ---\n649,640,665\n682,-795,504\n-784,533,-524\n-644,584,-595\n-588,-843,648\n-30,6,44\n-674,560,763\n500,723,-460\n609,671,-379\n-555,-800,653\n-675,-892,-343\n697,-426,-610\n578,704,681\n493,664,-388\n-671,-858,530\n-667,343,800\n571,-461,-707\n-138,-166,112\n-889,563,-600\n646,-828,498\n640,759,510\n-630,509,768\n-681,-892,-333\n673,-379,-804\n-742,-814,-386\n577,-820,562\n\n--- scanner 3 ---\n-589,542,597\n605,-692,669\n-500,565,-823\n-660,373,557\n-458,-679,-417\n-488,449,543\n-626,468,-788\n338,-750,-386\n528,-832,-391\n562,-778,733\n-938,-730,414\n543,643,-506\n-524,371,-870\n407,773,750\n-104,29,83\n378,-903,-323\n-778,-728,485\n426,699,580\n-438,-605,-362\n-469,-447,-387\n509,732,623\n647,635,-688\n-868,-804,481\n614,-800,639\n595,780,-596\n\n--- scanner 4 ---\n727,592,562\n-293,-554,779\n441,611,-461\n-714,465,-776\n-743,427,-804\n-660,-479,-426\n832,-632,460\n927,-485,-438\n408,393,-506\n466,436,-512\n110,16,151\n-258,-428,682\n-393,719,612\n-211,-452,876\n808,-476,-593\n-575,615,604\n-485,667,467\n-680,325,-822\n-627,-443,-432\n872,-547,-609\n833,512,582\n807,604,487\n839,-516,451\n891,-625,532\n-652,-548,-490\n30,-46,-14";
+        let split = split_lines_by_blanks(&input);
+        let scanner_map: FxHashMap<usize, Vec<IVec3>> =
+            split.iter().map(|x| parse_scanner(x)).collect();
+
+        let result = puzzle_b(&scanner_map);
+        assert_eq!(result, 3621);
     }
 }
