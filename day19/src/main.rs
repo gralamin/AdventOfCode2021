08f7528fea2e2ca7f9c292202240d876359a06d7
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-19 14:35:58 -0700

    Day19 part1: very slow solution

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,3 +1,19 @@
+use day19::parse_scanner;
+use day19::puzzle_a;
+use day19::{load, split_lines_by_blanks};
+use day19::{FxHashMap, IVec3};
+//use day19::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+    let split = split_lines_by_blanks(&all_lines);
+    let scanner_map: FxHashMap<usize, Vec<IVec3>> =
+        split.iter().map(|x| parse_scanner(x)).collect();
+
+    let value_a = puzzle_a(&scanner_map);
+    println!("Solution to 1: {}", value_a);
+
+    //let value_b = puzzle_b();
+    //println!("Solution to 2: {}", value_b);
 }
