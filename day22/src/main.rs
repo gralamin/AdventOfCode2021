3864dc29b4b44e9ca025217acb63978897ee30d1
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-21 23:12:35 -0700

    Day22: Super naive answer, but it works

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,16 @@
+use day22::load;
+use day22::parse_instructions;
+use day22::puzzle_a;
+// use day22::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+    let instructions = parse_instructions(&all_lines);
+
+    let value_a = puzzle_a(&instructions);
+    println!("Solution to 1: {}", value_a);
+
+    // let value_b = puzzle_b(&instructions);
+    // println!("Solution to 2: {}", value_b);
 }
