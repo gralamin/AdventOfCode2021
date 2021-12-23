ad5b3b4ed5c8d82c98abe433f50418d8735ff638
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-23 16:59:11 -0700

    Day23: Steal a solution, with a bit of work

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,3 +1,16 @@
+use day23::load;
+use day23::parse_amphipod;
+use day23::puzzle_a;
+use day23::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+    let amphipod = parse_amphipod(&all_lines);
+
+    let value_a = puzzle_a(&amphipod);
+    println!("Solution to 1: {}", value_a);
+
+    let value_b = puzzle_b(&amphipod);
+    println!("Solution to 2: {}", value_b);
 }
