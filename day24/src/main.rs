a2a95ed562b1a7f89a76d75bf7e0ae697e4869cb
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-23 23:37:43 -0700

    Day24: Super naive solution

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,3 +1,16 @@
+use day24::load;
+use day24::parse_all_instructions;
+use day24::puzzle_a;
+//use day24::puzzle_b;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+    let all_lines = load(filename);
+    let instructions = parse_all_instructions(&all_lines);
+
+    let value_a = puzzle_a(&instructions);
+    println!("Solution to 1: {}", value_a);
+
+    //let value_b = puzzle_b(&instructions);
+    //println!("Solution to 2: {}", value_b);
 }
