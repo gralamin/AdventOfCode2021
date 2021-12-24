0e0499f654e8ec4e09b4eab3bec428adb2d63e82
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-24 02:08:44 -0700

    Day24: Solution 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,16 +1,28 @@
 use day24::load;
 use day24::parse_all_instructions;
 use day24::puzzle_a;
-//use day24::puzzle_b;
+use day24::puzzle_b;
+use day24::Cache;
 
 fn main() {
     let filename = "input";
     let all_lines = load(filename);
     let instructions = parse_all_instructions(&all_lines);
 
-    let value_a = puzzle_a(&instructions);
+    let mut cache = Cache::default();
+
+    let value_a = puzzle_a(&instructions, &mut cache);
     println!("Solution to 1: {}", value_a);
 
-    //let value_b = puzzle_b(&instructions);
-    //println!("Solution to 2: {}", value_b);
+    let value_b = puzzle_b(&instructions, &mut cache);
+    println!("Solution to 2: {}", value_b);
 }
+
+/*
+Performance without caching (both parts):
+
+
+
+Performance with caching (both parts): TODO
+
+*/
