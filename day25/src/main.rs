de6daefca39820bfc9f506fc5b163ab6e2554cb1
Glen Nelson <glen.nelson.1@gmail.com>
2021-12-24 23:15:04 -0700

    Day25: Clean up because no part 2

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,8 +1,6 @@
 use day25::load;
 use day25::parse_sea_cucs;
 use day25::puzzle_a;
-//use day24::puzzle_b;
-//use day24::Cache;
 
 fn main() {
     let filename = "input";
@@ -12,11 +10,6 @@ fn main() {
     assert_eq!(cucumbers.len(), 137);
     assert_eq!(cucumbers[0].len(), 139);
 
-    //let mut cache = Cache::default();
-
     let value_a = puzzle_a(&cucumbers);
     println!("Solution to 1: {}", value_a);
-
-    // let value_b = puzzle_b(&cucumbers);
-    // println!("Solution to 2: {}", value_b);
 }
